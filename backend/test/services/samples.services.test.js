6e42b857f5865feb16d02a5d4c010583d49b7fb3
Andreas Laghamn <andreas.laghamn@gmail.com>
2020-02-08 23:45:17 +0100

    Add first unit test in CI

---
+++ backend/test/services/samples.services.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+
+describe('Pass', () => {
+  it('should true be equal to true', () => {
+    expect(true).to.equal(true);
+  });
+});
