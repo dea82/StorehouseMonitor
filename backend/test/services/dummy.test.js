06bebaaecd5726cd5ba61746b0f471e27249548f
Andreas <andreas.laghamn@gmail.com>
2020-02-11 22:20:26 +0100

    Add Jest test CI (#2)
    * Add Jest test CI
    
    Fix #1

---
+++ backend/test/services/dummy.test.js
@@ -0,0 +1,6 @@
+describe('dummy test suite', () => {
+  it('true is true', () => {
+    expect.assertions(1);
+    expect(true).toBe(true);
+  });
+});
--- backend/test/services/samples.services.test.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai');
-const { describe, it } = require('mocha');
-
-describe('Pass', () => {
-  it('should true be equal to true', () => {
-    expect(true).to.equal(true);
-  });
-});
