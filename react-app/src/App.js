e7a36e57598f41c52902a4828d560a33f60b3574
Andreas Laghamn <andreas.laghamn@gmail.com>
2020-02-18 20:48:39 +0100

    Updated frontend to match smaller screens

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,91 +1,151 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
+import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import DeveloperBoardIcon from "@material-ui/icons/DeveloperBoard";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import TimelineIcon from "@material-ui/icons/Timeline";
 import InfoIcon from "@material-ui/icons/Info";
-import { Link } from "react-router-dom";
-import MainPage from "./Components/MainPage";
+import {Link} from "react-router-dom"
+import MainPage from "./Components/MainPage"
 
-const drawerWidth = 220;
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex"
   },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1
-  },
   drawer: {
-    width: drawerWidth,
-    flexShrink: 0
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
   },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3)
-  },
-  toolbar: theme.mixins.toolbar
+  }
 }));
 
-export default function ClippedDrawer() {
+export default function ResponsiveDrawer(props) {
+  const { container } = props;
   const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        <ListItem button key="Sensors" component={Link} to="/sensors">
+          <ListItemIcon>
+            <DeveloperBoardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Sensors" />
+        </ListItem>
+        <ListItem button key="Plots" component={Link} to="/plots">
+          <ListItemIcon>
+            <TimelineIcon />
+          </ListItemIcon>
+          <ListItemText primary="Plots" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button key="Status" component={Link} to="/status">
+          <ListItemIcon>
+            <InfoIcon />
+          </ListItemIcon>
+          <ListItemText primary="Status" />
+        </ListItem>
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Link to="/" style={{ color: "inherit", textDecoration: "inherit" }}>
             <Typography variant="h6" noWrap>
               Storehouse Monitor
             </Typography>
           </Link>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper
-        }}
-      >
-        <div className={classes.toolbar} />
-        <List>
-          <ListItem button key="Sensors" component={Link} to="/sensors">
-            <ListItemIcon>
-              <DeveloperBoardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Sensors" />
-          </ListItem>
-          <ListItem button key="Plots" component={Link} to="/plots">
-            <ListItemIcon>
-              <TimelineIcon />
-            </ListItemIcon>
-            <ListItemText primary="Plots" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem button key="Status" component={Link} to="/status">
-            <ListItemIcon>
-              <InfoIcon />
-            </ListItemIcon>
-            <ListItemText primary="Status" />
-          </ListItem>
-        </List>
-      </Drawer>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <MainPage />
@@ -93,3 +153,10 @@ export default function ClippedDrawer() {
     </div>
   );
 }
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  container: PropTypes.any
+};
